b93aafec587c7e59a61de4e23b86f1e29bae54d7
Jack Clarke <me@jackclarke.xyz>
2019-04-30 04:25:28 +0100

    automatic yes for build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ build_depends="libX11-devel libXinerama-devel libXft-devel"
 build_remove="libXinerama-devel libXft-devel"
 depends="libXinerama libXft libX11"
 
-sudo xbps-install -S $build_depends $depends
+sudo xbps-install -yS $build_depends $depends
 
 make clean
 make
 
-sudo xbps-remove -R $build_remove
+sudo xbps-remove -yR $build_remove
